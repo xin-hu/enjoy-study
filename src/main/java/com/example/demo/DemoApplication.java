79a7ab2688d3a63f3ccdbd2e92f59e20384120f3
xin-hu <dfshcc@126.com>
2019-04-03 22:51:46 +0800

    v9.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/demo")
     public String getMessage(){
-        return "hello xiao xi fu";
+        return "hello xiao xi fu --master v8.0";
     }
 
     public static void main(String[] args) {
